bcff8a2baf1cd1813b7791536a9af1447ddf5524
zh-deng <heromemo@web.de>
2025-10-13 18:07:45 +0200

    chore(husky): update pre-commit and commit-msg files to match husky v10

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "../styles/main.scss"
+import "../styles/main.scss";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
