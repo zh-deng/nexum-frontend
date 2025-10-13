0ba160a3a6e980b2b246100168eb4602c14bf22c
zh-deng <heromemo@web.de>
2025-10-13 22:06:16 +0200

    feat: add singup page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,18 +2,18 @@ import Link from "next/link";
 import "./signup.scss";
 
 export default function SignInPage() {
-	return (
-		<main className="signup-page">
-			<h1>Sign Up</h1>
-			<form>
+  return (
+    <main className="signup-page">
+      <h1>Sign Up</h1>
+      <form>
         <input type="text" placeholder="First Name" required />
         <input type="text" placeholder="Last Name" required />
-				<input type="email" placeholder="Email" required />
-				<input type="password" placeholder="Password" required />
+        <input type="email" placeholder="Email" required />
+        <input type="password" placeholder="Password" required />
         <input type="password" placeholder="Repeat-Password" required />
-				<button type="submit">Sign Up</button>
-			</form>
-			<Link href="/signin">You have an account already? Sign In!</Link>
-		</main>
-	);
+        <button type="submit">Sign Up</button>
+      </form>
+      <Link href="/signin">You have an account already? Sign In!</Link>
+    </main>
+  );
 }
