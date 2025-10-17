887cc0cb8e612c43ff25cd32792f3b7bb1793797
zh-deng <heromemo@web.de>
2025-10-17 21:20:15 +0200

    feat: add login form component

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,34 +3,34 @@
 import Link from "next/link";
 import "./signup.scss";
 import SignupForm, {
-	SignUpFormData,
+  SignUpFormData,
 } from "../../components/auth/SingupForm/SignupForm";
 import { signupUser } from "../../lib/api/auth";
 import { useRouter } from "next/navigation";
 
 const SignupPage = () => {
-	const router = useRouter();
+  const router = useRouter();
 
-	async function handleSignup(data: SignUpFormData) {
-		const { repeatPassword, ...payload } = data;
+  async function handleSignup(data: SignUpFormData) {
+    const { repeatPassword, ...payload } = data;
 
-		try {
-			await signupUser(payload);
-			router.push("/login");
-		} catch (error: any) {
-			console.error("Signup error:", error);
-		}
-	}
+    try {
+      await signupUser(payload);
+      router.push("/login");
+    } catch (error: unknown) {
+      console.error("Signup error:", error);
+    }
+  }
 
-	return (
-		<div className="signup-page">
-			<div className="form-container">
-				<h1>Sign Up</h1>
-				<SignupForm onSubmit={handleSignup} />
-				<Link href="/login">You have an account already? Log in!</Link>
-			</div>
-		</div>
-	);
+  return (
+    <div className="signup-page">
+      <div className="form-container">
+        <h1>Sign Up</h1>
+        <SignupForm onSubmit={handleSignup} />
+        <Link href="/login">You have an account already? Log in!</Link>
+      </div>
+    </div>
+  );
 };
 
 export default SignupPage;
---
+++ src/components/auth/LoginForm/LoginForm.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import "./LoginForm.scss";
+import { Button, TextField } from "@radix-ui/themes";
+
+export type LoginFormData = {
+  email: string;
+  password: string;
+};
+
+type LoginFormProps = {
+  onSubmit: (data: LoginFormData) => Promise<void>;
+  defaultValues?: Partial<LoginFormData>;
+};
+
+const LoginForm = ({ onSubmit, defaultValues }: LoginFormProps) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<LoginFormData>({
+    defaultValues,
+  });
+
+  return (
+    <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <TextField.Root
+          className="radix-textfield"
+          placeholder="Email"
+          type="email"
+          {...register("email", { required: "Email is required" })}
+        />
+        {errors.email && <span className="error">{errors.email.message}</span>}
+      </div>
+      <div>
+        <TextField.Root
+          className="radix-textfield"
+          placeholder="Password"
+          type="password"
+          {...register("password", { required: "Password is required" })}
+        />
+        {errors.password && (
+          <span className="error">{errors.password.message}</span>
+        )}
+      </div>
+      <Button type="submit" mt={"6"} disabled={isSubmitting}>
+        Log In
+      </Button>
+    </form>
+  );
+};
+
+export default LoginForm;
