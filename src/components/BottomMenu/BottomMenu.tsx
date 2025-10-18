22b8e74a6d8acfab8918a739b634b2a836fabe7f
zh-deng <heromemo@web.de>
2025-10-18 15:48:27 +0200

    feat: add bottom menu component

--- src/components/BottomMenu/BottomMenu.tsx
+++ src/components/BottomMenu/BottomMenu.tsx
@@ -6,57 +6,57 @@ import { Separator } from "@radix-ui/themes";
 import { useState } from "react";
 
 type MenuItem = {
-	name: string;
-	route: string;
+  name: string;
+  route: string;
 };
 
 const BottomMenu = () => {
-	const router = useRouter();
-	const menuItems: MenuItem[] = [
-		{
-			name: "Jobs",
-			route: "jobs",
-		},
-		{
-			name: "Interviews",
-			route: "interviews",
-		},
-		{
-			name: "Reminders",
-			route: "reminders",
-		},
-		{
-			name: "Statistics",
-			route: "statistics",
-		},
-	];
-	const [activeItem, setActiveItem] = useState<string>("jobs");
+  const router = useRouter();
+  const menuItems: MenuItem[] = [
+    {
+      name: "Jobs",
+      route: "jobs",
+    },
+    {
+      name: "Interviews",
+      route: "interviews",
+    },
+    {
+      name: "Reminders",
+      route: "reminders",
+    },
+    {
+      name: "Statistics",
+      route: "statistics",
+    },
+  ];
+  const [activeItem, setActiveItem] = useState<string>("jobs");
 
-	function navigateTo(route: string) {
-		router.push(`/dashboard/${route}`);
-		setActiveItem(route);
-	}
+  function navigateTo(route: string) {
+    router.push(`/dashboard/${route}`);
+    setActiveItem(route);
+  }
 
-	return (
-		<div className="button-menu">
-			{menuItems.map((item, index) => {
-				return (
-					<>
-						<div
-							className={`menu-item ${activeItem === item.route ? "active-item" : ""}`}
-							key={item.name}
-							onClick={() => navigateTo(item.route)}
-						>
-							{item.name}
-						</div>
-						{index !== menuItems.length - 1 && (
-							<Separator orientation="vertical" size={"4"} />
-						)}
-					</>
-				);
-			})}
-		</div>
-	);
+  return (
+    <div className="button-menu">
+      {menuItems.map((item, index) => {
+        return (
+          <>
+            <div
+              className={`menu-item ${activeItem === item.route ? "active-item" : ""}`}
+              key={item.name}
+              onClick={() => navigateTo(item.route)}
+            >
+              {item.name}
+            </div>
+            {index !== menuItems.length - 1 && (
+              <Separator orientation="vertical" size={"4"} />
+            )}
+          </>
+        );
+      })}
+    </div>
+  );
 };
 
 export default BottomMenu;
