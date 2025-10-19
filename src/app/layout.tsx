a8fc4c9aa9e80d4a30a4cf0f24c7e792cb71913d
zh-deng <heromemo@web.de>
2025-10-19 15:13:43 +0200

    feat: add react query to the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import "@radix-ui/themes/styles.css";
 import { Theme } from "@radix-ui/themes";
 import Navbar from "../components/Navbar/Navbar";
 import ClientLayout from "./ClientLayout";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "../lib/react-query";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,18 +31,20 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <Theme
-          appearance="light"
-          accentColor="mint"
-          grayColor="slate"
-          radius="large"
-          scaling="95%"
-        >
-          <ClientLayout>
-            <Navbar />
-            {children}
-          </ClientLayout>
-        </Theme>
+        <QueryClientProvider client={queryClient}>
+          <Theme
+            appearance="light"
+            accentColor="mint"
+            grayColor="slate"
+            radius="large"
+            scaling="95%"
+          >
+            <ClientLayout>
+              <Navbar />
+              {children}
+            </ClientLayout>
+          </Theme>
+        </QueryClientProvider>
       </body>
     </html>
   );
