987bd81f403524e77b68e05e1af7bb68d77fd7f0
zh-deng <heromemo@web.de>
2025-10-19 15:14:11 +0200

    feat: add enums file

---
+++ src/types/enums.ts
@@ -0,0 +1,24 @@
+export enum Priority {
+  LOW = "LOW",
+  MEDIUM = "MEDIUM",
+  HIGH = "HIGH",
+}
+
+export enum ApplicationStatus {
+  DRAFT = "DRAFT",
+  APPLIED = "APPLIED",
+  INTERVIEW = "INTERVIEW",
+  OFFER = "OFFER",
+  HIRED = "HIRED",
+  DECLINED_OFFER = "DECLINED_OFFER",
+  REJECTED = "REJECTED",
+  GHOSTED = "GHOSTED",
+  WITHDRAWN = "WITHDRAWN",
+}
+
+export enum WorkLocation {
+  ON_SITE = "ON_SITE",
+  REMOTE = "REMOTE",
+  HYBRID = "HYBRID",
+  UNSURE = "UNSURE",
+}
