2653e1e0727217dfedb04f68e32e39d6788a315c
zh-deng <heromemo@web.de>
2025-10-27 14:54:26 +0100

    feat: add confirmation modal component

---
+++ src/components/ConfirmationModal/ConfirmationModal.tsx
@@ -0,0 +1,48 @@
+import { Button, Flex, Text } from "@radix-ui/themes";
+import "./ConfirmationModal.scss";
+import { useEffect } from "react";
+
+type ConfirmationModalProps = {
+  questionString?: string;
+  confirmationString?: string;
+  abortionString?: string;
+  isOpen: boolean;
+  onConfirmation: () => void;
+  onAbortion: () => void;
+};
+
+const ConfirmationModal = ({
+  isOpen,
+  questionString = "Are you sure?",
+  confirmationString = "YES",
+  abortionString = "NO",
+  onConfirmation,
+  onAbortion,
+}: ConfirmationModalProps) => {
+  function confirmAndClose() {
+    onConfirmation();
+    onAbortion();
+  }
+
+  if (!isOpen) return null;
+
+  return (
+    <div className="confirmation-modal">
+      <Flex direction={"column"} gap={"6"} align={"center"}>
+        <Text size={"5"} weight={"bold"}>
+          {questionString}
+        </Text>
+        <Flex gap={"7"}>
+          <Button size={"4"} onClick={confirmAndClose}>
+            {confirmationString}
+          </Button>
+          <Button size={"4"} onClick={onAbortion}>
+            {abortionString}
+          </Button>
+        </Flex>
+      </Flex>
+    </div>
+  );
+};
+
+export default ConfirmationModal;
