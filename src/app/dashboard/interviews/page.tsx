34e4d61b954ad1d3873767cd8d5f2d9e504062b6
zh-deng <heromemo@web.de>
2025-11-06 23:01:40 +0100

    feat(interview): implement logic to render interview cards

--- src/app/dashboard/interviews/page.tsx
+++ src/app/dashboard/interviews/page.tsx
@@ -1,10 +1,21 @@
+"use client";
+
 import ComingUpCard from "../../../components/ComingUpCard/ComingUpCard";
+import InterviewCard from "../../../components/InterviewCard/InterviewCard";
+import { useInterviews } from "../../../hooks/interview/useInterview";
 import "./interviews.scss";
 
 const InterviewsPage = () => {
+  const { data } = useInterviews();
+
   return (
     <div className="interviews-page">
-      <ComingUpCard />
+      <div>Sort by old/new, include exclude done</div>
+      <div className="interview-container">
+        {data.map((item) => {
+          return <InterviewCard key={item.id} data={item} />;
+        })}
+      </div>
     </div>
   );
 };
