c68c956678536f9157cb3b69a36be51957b218bf
zh-deng <heromemo@web.de>
2025-11-23 16:00:32 +0100

    refactor(bottom-menu): clean code

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -1,3 +1,3 @@
 export const API_BASE = `${process.env.NEXT_PUBLIC_API_URL}/api`;
-export const DEMO_EMAIL = process.env.DEMO_EMAIL || "";
-export const DEMO_PASSWORD = process.env.DEMO_PASSWORD || "";
+export const DEMO_EMAIL = process.env.NEXT_PUBLIC_DEMO_EMAIL || "";
+export const DEMO_PASSWORD = process.env.NEXT_PUBLIC_DEMO_PASSWORD || "";
