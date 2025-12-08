370475ac47cfecda7b7d0a78c43330540f473344
zh-deng <heromemo@web.de>
2025-12-08 22:23:23 +0100

    feat(job-settings): add follow up option

--- src/components/JobSettings/JobSettings.tsx
+++ src/components/JobSettings/JobSettings.tsx
@@ -22,22 +22,22 @@ const ghostedTimeOptions = [
   "12 Weeks",
 ];
 
+const followUpTimeOptions = ["1 Week", "2 Weeks", "3 Weeks"];
+
+// TODO upcoming feature
 const JobSettings = () => {
-  const [period, setPeriod] = useState<string>("4 Weeks");
-  // TODO add options entity to backend and frontend
+  const [ghostingPeriod, setGhostingPeriod] = useState<string>("4 Weeks");
   const [ghostingEnabled, setGhostingEnabled] = useState<boolean>(false);
+  const [followUpPeriod, setFollowUpPeriod] = useState<string>("2 Weeks");
+  const [followUpEnabled, setFollowUpEnabled] = useState<boolean>(false);
 
   const { isSm } = useBreakpoint();
-
-  function handleGhostingToggle() {
-    setGhostingEnabled(!ghostingEnabled);
-  }
 
   return (
     <Dialog.Root>
       <Dialog.Trigger>
-        <IconButton size={"4"} style={{ cursor: "pointer" }}>
-          <GearIcon width={32} height={32} />
+        <IconButton size={"3"} style={{ cursor: "pointer" }} radius={"small"}>
+          <GearIcon width={24} height={24} />
         </IconButton>
       </Dialog.Trigger>
 
@@ -63,14 +63,14 @@ const JobSettings = () => {
               justify={"between"}
               gap={"2"}
             >
-              <Text>Automatically Ghosted Status after</Text>
+              <Text>Mark as ghosted after</Text>
               <Flex align={"center"} gap={"2"} width={"fit-content"}>
                 <Box style={{ flex: "1" }}>
                   <Dropdown
                     disabled={!ghostingEnabled}
-                    name={period}
+                    name={ghostingPeriod}
                     options={ghostedTimeOptions}
-                    onChange={(selected) => setPeriod(selected)}
+                    onChange={(selected) => setGhostingPeriod(selected)}
                     width={isSm ? "100px" : "200px"}
                   />
                 </Box>
@@ -78,7 +78,32 @@ const JobSettings = () => {
                   style={{ cursor: "pointer" }}
                   size={"3"}
                   checked={ghostingEnabled}
-                  onCheckedChange={handleGhostingToggle}
+                  onCheckedChange={() => setGhostingEnabled(!ghostingEnabled)}
+                />
+              </Flex>
+            </Flex>
+            <Flex
+              direction={isSm ? "row" : "column"}
+              align={isSm ? "center" : "start"}
+              justify={"between"}
+              gap={"2"}
+            >
+              <Text>Remind to follow up interview after</Text>
+              <Flex align={"center"} gap={"2"} width={"fit-content"}>
+                <Box style={{ flex: "1" }}>
+                  <Dropdown
+                    disabled={!followUpEnabled}
+                    name={followUpPeriod}
+                    options={followUpTimeOptions}
+                    onChange={(selected) => setFollowUpPeriod(selected)}
+                    width={isSm ? "100px" : "200px"}
+                  />
+                </Box>
+                <Switch
+                  style={{ cursor: "pointer" }}
+                  size={"3"}
+                  checked={followUpEnabled}
+                  onCheckedChange={() => setFollowUpEnabled(!followUpEnabled)}
                 />
               </Flex>
             </Flex>
